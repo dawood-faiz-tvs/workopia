5ba5985eb2d4ad5beba185682ed5952abf6b7e9a
dawood-faiz-tvs <dawood.faiz.tvs@gmail.com>
2024-01-22 21:42:39 +0500

    Refactoring loadView() and loadPartial()

--- helpers.php
+++ helpers.php
@@ -3,3 +3,25 @@ function basePath($path = "")
 {
     return __DIR__ . "/" . $path;
 }
+
+function loadView($name)
+{
+    $viewPath = basePath("views/{$name}.view.php");
+
+    if (file_exists($viewPath)) {
+        require($viewPath);
+    } else {
+        echo "View {$name} not found!";
+    }
+}
+
+function loadPartial($name)
+{
+    $partialPath = basePath("views/partials/{$name}.php");
+
+    if (file_exists($partialPath)) {
+        require($partialPath);
+    } else {
+        echo "View {$name} not found!";
+    }
+}
